e34699906ae9644b31a6c89bae8ae74dc42431c9
evanchenjj <evanchenjj@gmail.com>
2020-10-11 19:01:24 +0800

    Site updated: 2020-10-11 19:01:21

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/Yafine/cdn@2.5/source/favicon.png");
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/evanchenjj/cdn@1.0.1/images/favicon.ico");
          document.title = 'ヽ(●-`Д´-)ノ看不见我！看不见我！';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/Yafine/cdn@2.5/source/favicon.png");
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/evanchenjj/cdn@1.0.1/images/favicon.ico");
          document.title = 'ヾ(Ő∀Ő3)ノ被你发现了！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
